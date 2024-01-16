6e2d66ca9917bf77efdfb2c2120050f6d68087e4
Ihyeon Shin <80164690+2hy2on@users.noreply.github.com>
2024-01-16 17:13:14 +0900

    [Silver III] Title: 퇴사, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,44 +1,12 @@
-n = int(input())
-work = []
-picked = []  # 뽑은 거 인덱스 넣기
+N = int(input())
 
-for i in range(n):
-    day, pay = map(int, input().split())
-    work.append([day, pay])
+schedule = [list(map(int, input().split())) for i in range(N)]
 
-def pick(picked, day):
-    res = 0
-    totalPay = 0
+dp = [0 for i in range(N+1)]
 
-    if day >= n:
-        for i in picked:
-            totalPay += work[i][1]
-        #print(picked)
-        return totalPay
-    
-    for i in range(day, n):
-        temp = i
-        # i += work[i][0]
-        i = temp + work[i][0]  # 이 부분을 수정하여 누적되지 않도록 함
-        if i <= n:
-            picked.append(temp)
-            res = max(res, pick(picked, i))
-            picked.pop()
-        if i > n:
-            res = max(res, pick(picked, i))    #하나만 뽑혔을 때를 생각함 
-    return res
-##11
-# 5 10
-# 5 10
-# 5 200
-# 5 10
-# 5 10
-# 5 10
-# 5 10
-# 5 10
-# 5 10
-# 5 10
-# 5 10
-# 200 
-#i <= n만 있으면 200이 나오지 않음
-print(pick(picked, 0))
+
+for i in range(N):
+    for j in range(i+schedule[i][0], N+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+print(dp[-1])
