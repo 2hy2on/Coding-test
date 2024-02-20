343dcf829a18dae9b48ddd5d5d95e796563d3be8
Ihyeon Shin <80164690+2hy2on@users.noreply.github.com>
2024-02-21 00:47:12 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,20 +1,19 @@
-N = int(input())
-# dp에 사용할 배열과 각 계단에 저장할 값을 선언
-dp = [0] * (N+1)
-point = [0] * (N+1)
-for i in range(1, N+1):
-    point[i] = int(input())
-if N==1:
-    print(point[1])
-    exit()
-elif N==2:
-    print(sum(point[:3]))  # 계단이 2개 이하일땐 그냥 다 더해서 출력
-    exit()
-dp[1] = point[1]
-dp[2] = point[1]+point[2]
-for i in range(3, N+1):
-            # 연속되지 않게 i번째 계단에 도착할 경우
-        # i번째 계단과 i-1번째 계단이 연속될 경우
-    dp[i] = max(dp[i-2]+point[i], dp[i-3]+point[i-1]+point[i])
+n = int(input())
+stair = [0] * (n+1)
+dp=[0] * (n+1)
+for i in range(1, n+1):
+    stair[i] = int(input())
 
-print(dp[-1])
+if len(stair)<=2: # 계단이 2개 이하일땐 그냥 다 더해서 출력
+    print(sum(stair))
+
+else:
+    dp[1]=stair[1] # 첫째 계단 수동 계산
+    dp[2]=stair[1]+stair[2] # 둘째 계단까지 수동 계산
+    for i in range(3, n+1):
+        # 연속되지 않게 i번째 계단에 도착할 경우
+        # i번째 계단과 i-1번째 계단이 연속될 경우
+        dp[i] = max(dp[i-2]+stair[i], dp[i-3]+stair[i-1]+stair[i])
+    print(dp[-1])
+
+# 너무 일반화하려함, 경우의 수를 차근히 살펴보고 구하기
