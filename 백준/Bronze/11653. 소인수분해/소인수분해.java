6a5ecc2760bb6b330c10c16d2ba029cdb9c9b949
Ihyeon Shin <80164690+2hy2on@users.noreply.github.com>
2024-11-05 22:52:45 +0900

    [Bronze I] Title: 소인수분해, Time: 136 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int prime_num = 2;
+
+        if (N == 1) {
+            return;
+        }
+
+        while(true) {
+
+            while(N % prime_num == 0) {
+                N /= prime_num;
+                System.out.println(prime_num);
+
+            }
+
+            if ( N != 1) {
+                prime_num++;
+            } else {
+                break;
+            }
+        }
+    }
+}
